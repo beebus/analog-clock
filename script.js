cb08ce2be579f39695ee89a8a8017e5d5968b13f
Michael Beebower <michael.beebower@g.austincc.edu>
2020-05-29 10:32:02 -0500

    Updated script.js to move clock hands

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 20;    // set initially to random numbers
+let minPosition = 130;
+let secPosition = 267;
+
+// apply these numbers as degrees
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
