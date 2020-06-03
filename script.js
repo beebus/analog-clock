6eab0023bfc5718f744b221701db3a09df27d0c4
Michael Beebower <michael.beebower@g.austincc.edu>
2020-06-03 10:22:36 -0500

    Updated script.js so that function runTheClock has the "return to zero" issue fixed.

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-function runTheClock() {
-    // get the current hour, minute, and second with the Date() object
-    var date = new Date();
-    console.log(date);
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+// get the current hour, minute, and second with the Date() object
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
-    // calculate the degree position for each hand of the clock
-    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
-    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
-    let secPosition = sec * 360 / 60;
+// calculate the degree position for each hand of the clock
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
+
+function runTheClock() {
+
+    hrPosition = hrPosition+(3/360);    // same as (30/3600)
+    minPosition = minPosition+(6/60);   // same as (1/60)*6
+    secPosition = secPosition+6;
 
     // apply these numbers as degrees
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
