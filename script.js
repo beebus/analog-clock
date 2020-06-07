d7bbd59e41ca2e063e0ff0babfae41f19b677046
Michael Beebower <michael.beebower@g.austincc.edu>
2020-06-07 15:16:43 -0500

    Linted with JSHint and ESLint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 // get the current hour, minute, and second with the Date() object
-var date = new Date();
+let date = new Date();
 console.log(date);
 let hr = date.getHours();
 let min = date.getMinutes();
@@ -17,15 +17,15 @@ let secPosition = sec * 360 / 60;
 
 function runTheClock() {
 
-    hrPosition = hrPosition+(3/360);    // same as (30/3600)
-    minPosition = minPosition+(6/60);   // same as (1/60)*6
-    secPosition = secPosition+6;
+	hrPosition = hrPosition+(3/360);    // same as (30/3600)
+	minPosition = minPosition+(6/60);   // same as (1/60)*6
+	secPosition = secPosition+6;
 
-    // apply these numbers as degrees
-    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+	// apply these numbers as degrees
+	HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+	MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+	SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
 // call the runTheClock function continuously
-var interval = setInterval(runTheClock, 1000);
+setInterval(runTheClock, 1000);
